70bb2518891f1496b9b1b0c52265cbfe418b0083
Ash Davies <ashdavies@users.noreply.github.com>
2019-06-10 23:41:12 +0200

    Include spread operator

--- src/main/kotlin/io/ashdavies/testing/LiveDataIterable.kt
+++ src/main/kotlin/io/ashdavies/testing/LiveDataIterable.kt
@@ -17,7 +17,7 @@ internal class LiveDataIterable<T> : LiveDataRegistry<T> {
   }
 
   override fun expect(vararg values: T) {
-    check(listOf(values) == history) { "Expecting values $values but it is actually $history" }
+    check(listOf(*values) == history) { "Expecting values $values but it is actually $history" }
   }
 
   override fun await() {
