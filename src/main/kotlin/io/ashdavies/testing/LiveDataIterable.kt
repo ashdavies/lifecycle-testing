fdbd58a381ccddc1b2a779950357125b7b57dc7e
Ash Davies <ashdavies@users.noreply.github.com>
2019-06-10 23:15:44 +0200

    Include additional behaviour for awaiting values

--- src/main/kotlin/io/ashdavies/testing/LiveDataIterable.kt
+++ src/main/kotlin/io/ashdavies/testing/LiveDataIterable.kt
@@ -1,8 +1,12 @@
 package io.ashdavies.testing
 
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
 internal class LiveDataIterable<T> : LiveDataRegistry<T> {
 
   private val history: MutableList<T> = mutableListOf()
+  private var latch = CountDownLatch(1)
 
   override fun iterator(): Iterator<T> {
     return history.iterator()
@@ -14,5 +18,17 @@ internal class LiveDataIterable<T> : LiveDataRegistry<T> {
 
   override fun expect(vararg values: T) {
     check(listOf(values) == history) { "Expecting values $values but it is actually $history" }
+  }
+
+  override fun await() {
+    latch.await()
+  }
+
+  override fun await(timeout: Long, unit: TimeUnit) {
+    latch.await(timeout, unit)
+  }
+
+  override fun reset() {
+    latch = CountDownLatch(1)
   }
 }
--- src/main/kotlin/io/ashdavies/testing/LiveDataRegistry.kt
+++ src/main/kotlin/io/ashdavies/testing/LiveDataRegistry.kt
@@ -1,8 +1,16 @@
 package io.ashdavies.testing
 
+import java.util.concurrent.TimeUnit
+
 interface LiveDataRegistry<T> : Iterable<T> {
 
   fun emit(vararg values: T)
 
   fun expect(vararg values: T)
+
+  fun await()
+
+  fun await(timeout: Long, unit: TimeUnit)
+
+  fun reset()
 }
